210e2ac758dfa1c8474b05ee48a81f94cd142ade
ManantsoaRayan <mananatsoarayan@gmail.com>
2025-12-14 17:49:38 +0300

    Refactor TsenaMora model and update lockfile
    Add safe authenticateUser that returns false if user is missing. Minor
    style and spacing cleanup in TsenaMora model. Update pubspec.lock: bump
    meta to 1.17.0 and test_api to 0.7.7 (with updated sha256 entries)

--- lib/model/tsenaMoraModel.dart
+++ lib/model/tsenaMoraModel.dart
@@ -1,30 +1,29 @@
-class TsenaMora{
+class TsenaMora {
   String userName;
   String password;
 
-  TsenaMora({
-    this.userName = '',
-    this.password = ''
-  });
+  TsenaMora({this.userName = '', this.password = ''});
 }
 
 class TsenaMoraModel {
-  List <TsenaMora> userList = [
+  List<TsenaMora> userList = [
     TsenaMora(userName: 'Alice', password: "1234"),
-    TsenaMora(userName: "Toky", password: "12345678")
+    TsenaMora(userName: "Toky", password: "12345678"),
   ];
 
-  List <TsenaMora> get getUserList => List.unmodifiable(userList);
+  List<TsenaMora> get getUserList => List.unmodifiable(userList);
 
   void addUser(String user, String password) {
-    userList.add(TsenaMora(userName: user, password:  password));
+    userList.add(TsenaMora(userName: user, password: password));
   }
 
+  bool authenticateUser(String userName, String password) {
+    try {
+      final userFound = userList.firstWhere((user) => user.userName == userName);
+      return userFound.password == password;
+    } catch (e) {
+      return false;
+    }
 
-  bool authenticateUser(String userName ,String password) {
-    final userFound  = userList.firstWhere(
-      (user) => user.userName == userName,
-    ); 
-    return userFound.password == password;
   }
 }
